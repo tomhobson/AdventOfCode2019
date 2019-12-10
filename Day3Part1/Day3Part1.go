75b96df364ac51c9a886fb34b1e3d587af3a422d
Tom Hobson <tomhobsonleeds@gmail.com>
2019-12-10 23:31:09 +0000

    Day3part1 completed

--- Day3Part1/Day3Part1.go
+++ Day3Part1/Day3Part1.go
@@ -23,33 +23,153 @@ func main() {
 	wire1Directions := strings.Split(wires[0], ",")
 	wire2Directions := strings.Split(wires[1], ",")
 
-	wire1 := convertDirectionsToMapOfCoordinates(wire1Directions)
-	wire2 := convertDirectionsToMapOfCoordinates(wire2Directions)
+	var wiremap map[int]map[int]WirePoint
+	wiremap = make(map[int]map[int]WirePoint)
+
+	// Do wire 1
+	currentX := 0
+	currentY := 0
+
+	// Init zero
+	wiremap[currentX] = make(map[int]WirePoint)
+
+	for _, dirAndLength := range wire1Directions {
+		newX := currentX
+		newY := currentY
+		length, _ := strconv.Atoi(dirAndLength[1:])
+		if dirAndLength[0] == 'U' {
+			newY = currentY - length
+		} else if dirAndLength[0] == 'D' {
+			newY = currentY + length
+		} else if dirAndLength[0] == 'L' {
+			newX = currentX - length
+		} else if dirAndLength[0] == 'R' {
+			newX = currentX + length
+		}
+
+		for currentX != newX {
+			if currentX > newX {
+				currentX = currentX - 1
+				if wiremap[currentX] == nil {
+					wiremap[currentX] = make(map[int]WirePoint)
+				}
+				wp := wiremap[currentX][currentY]
+				wp.Wire1HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else if currentX < newX {
+				currentX = currentX + 1
+				if wiremap[currentX] == nil {
+					wiremap[currentX] = make(map[int]WirePoint)
+				}
+				wp := wiremap[currentX][currentY]
+				wp.Wire1HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else {
+				break
+			}
+		}
+
+		for currentY != newY {
+			if currentY > newY {
+				currentY = currentY - 1
+				wp := wiremap[currentX][currentY]
+				wp.Wire1HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else if currentY < newY {
+				currentY = currentY + 1
+				wp := wiremap[currentX][currentY]
+				wp.Wire1HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else {
+				break
+			}
+		}
+	}
+
+	//Do wire 2
+	currentX = 0
+	currentY = 0
+
+	for _, dirAndLength := range wire2Directions {
+		newX := currentX
+		newY := currentY
+		length, _ := strconv.Atoi(dirAndLength[1:])
+		if dirAndLength[0] == 'U' {
+			newY = currentY - length
+		} else if dirAndLength[0] == 'D' {
+			newY = currentY + length
+		} else if dirAndLength[0] == 'L' {
+			newX = currentX - length
+		} else if dirAndLength[0] == 'R' {
+			newX = currentX + length
+		}
+
+		for currentX != newX {
+			if currentX > newX {
+				currentX = currentX - 1
+				if wiremap[currentX] == nil {
+					wiremap[currentX] = make(map[int]WirePoint)
+				}
+				wp := wiremap[currentX][currentY]
+				wp.Wire2HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else if currentX < newX {
+				currentX = currentX + 1
+				if wiremap[currentX] == nil {
+					wiremap[currentX] = make(map[int]WirePoint)
+				}
+				wp := wiremap[currentX][currentY]
+				wp.Wire2HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else {
+				break
+			}
+		}
+
+		for currentY != newY {
+			if currentY > newY {
+				currentY = currentY - 1
+				wp := wiremap[currentX][currentY]
+				wp.Wire2HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else if currentY < newY {
+				currentY = currentY + 1
+				wp := wiremap[currentX][currentY]
+				wp.Wire2HasHit = true
+				wiremap[currentX][currentY] = wp
+			} else {
+				break
+			}
+		}
+	}
+
+	lowestdistance := 100000
+	// The issue here is that I don't care if it's itself or another
+	for x, map1 := range wiremap {
+		for y, yval := range map1 {
+			if yval.Wire1HasHit && yval.Wire2HasHit {
+				currentDis := intAbs(x) + intAbs(y)
+				if currentDis < lowestdistance {
+					lowestdistance = currentDis
+				}
+			}
+		}
+	}
+
+	fmt.Println("Lowest Distance:", lowestdistance)
 
 	elapsed := time.Since(start)
 	fmt.Println("Execution Time: %s", elapsed)
 }
 
-func convertDirectionsToMapOfCoordinates(directions []string) map[int]wireCoordinate {
-	currentX := 0
-	currentY := 0
-
-	var wire map[int]wireCoordinate
-	for _, dirAndLength := range directions {
-		length = strconv.Atoi(dirAndLength[1:])
-		if dirAndLength[0] == 'U' {
-
-		} else if dirAndLength[0] == 'D' {
-
-		} else if dirAndLength[0] == 'L' {
-
-		} else if dirAndLength[0] == 'R' {
-
-		}
-	}
+type WirePoint struct {
+	Wire1HasHit bool
+	Wire2HasHit bool
 }
 
-type wireCoordinate struct {
-	x int
-	y int
+func intAbs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
